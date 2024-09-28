b328d7a260514bcd17ddf337eb745ab1690c416e
Andres <andresvz50@gmail.com>
2024-09-27 22:23:04 -0500

    finish top-navbar component

--- src/app/top-navbar/top-navbar.component.ts
+++ src/app/top-navbar/top-navbar.component.ts
@@ -8,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './top-navbar.component.css'
 })
 export class TopNavbarComponent {
-
+  infoIcon = "fas fa-info-circle";
+  infoTitle = "The Future in a Construction. WorldWide.";
+  socialLinks = [
+    { id: 1, icon: 'fab fa-facebook-f', url: 'https://www.facebook.com' },
+    { id: 2, icon: 'fab fa-twitter', url: 'https://www.twitter.com' },
+    { id: 3, icon: 'fab fa-linkedin', url: 'https://www.linkedin.com' },
+    { id: 4, icon: 'fab fa-instagram', url: 'https://www.instagram.com' }
+  ];
 }
