a06a6148e062ac0149b3a77ae73ba9ebffc1cd33
Andres <andresvz50@gmail.com>
2024-09-27 21:36:34 -0500

    create top-navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { TopNavbarComponent } from './top-navbar/top-navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet, TopNavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
