e42704f62f2d827bae4d630ac3c2c97753f81696
Andres <andresvz50@gmail.com>
2024-10-01 17:26:52 -0500

    last changes for main-navbar component

--- src/app/main-navbar/main-navbar.component.ts
+++ src/app/main-navbar/main-navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './main-navbar.component.html',
   styleUrl: './main-navbar.component.css'
 })
